adce28c2bbb58fda96b0c4c574606f7d685904d2
Arthur Himmel <himmel.arthur@gmail.com>
2014-01-05 22:43:14 +0100

    First message working

--- rcemdjango/stream/urls.py
+++ rcemdjango/stream/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from stream import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^message/add$', views.add_message, name='add_message')
 )
--- rcemdjango/stream/views.py
+++ rcemdjango/stream/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from stream.models import Message
+from stream.models import Message, Utilisateur
 
 def index(request):
     latest_message_list = Message.objects.all().order_by('date')[:5]
     context = {'latest_message_list': latest_message_list}
     return render(request, 'stream/index.html', context)
+
+def add_message(request):
+    # Si la requete n'est pas un POST, rediriger au stream
+    if request.method != 'POST':
+        return redirect('/stream')
+
+    message_content = request.POST.get('message', None)
+
+    if message_content != None: # Si il y a bien un message, on l'enregistre
+        user = Utilisateur.ghost_user()
+        message_content = message_content.strip() # On enleve le whitespace du message
+        message = Message.objects.create(auteur=user, texte=message_content)
+        message.save()
+
+    return redirect('/stream')
