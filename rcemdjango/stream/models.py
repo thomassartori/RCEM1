f6065579a840f54354abe301a10191c229a8df2c
thomas <thomas.j.sartori@gmail.com>
2014-01-05 21:31:35 +0100

    2

--- rcemdjango/stream/models.py
+++ rcemdjango/stream/models.py
@@ -3,11 +3,15 @@ from django.db import models
 class Utilisateur(models.Model):
     nom = models.CharField(max_length=200)
     profession =  models.CharField(max_length=200)
-    lieu = 
-    email = models.CharField(max_length=50)
+    lieu = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
 
 
 class Message(models.Model):
     auteur = models.ForeignKey(Utilisateur)
     texte = models.CharField(max_length=500)
     date = models.DateTimeField('date published')
+
+class Filtre(models.Model):
+	lieu = models.CharField(max_length=200)
+	profession = models.CharField(max_length=200)    
--- rcemdjango/stream/views.py
+++ rcemdjango/stream/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render
+
+from stream.models import Message
+
+def index(request):
+    latest_message_list = Message.objects.all().order_by('date')[:5]
+    context = {'latest_message_list': latest_message_list}
+    return render(request, 'stream/index.html', context)
